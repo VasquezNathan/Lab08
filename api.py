1b76823ef666e2573bf15098b275a3a2d84dd26b
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2021-11-16 14:19:30 -0800

    now able to access user db

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app) 
 
 class User(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, auto_increment = True) 
     username = db.Column(db.String, unique=True, nullable=False) 
     email = db.Column(db.String, unique=True, nullable=False)
--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask 
 from flask_admin import Admin 
-from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib import sqla
 from api import *
-app = Flask(__name__) 
+
 app.secret_key = 'super secret key'
+
 # set optional bootswatch theme
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-admin = Admin(app, name='microblog', template_mode='bootstrap3') 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.sqlite'
+# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
-admin.add_view(ModelView(User, db.session)) 
+# admin.add_view(ModelView(User, db.session)) 
 
+class UserView(sqla.ModelView): 
+    can_delete = False # disable model deletion 
+    can_create = False # disable model creation 
+    can_edit = False # disable model editing 
+
+
+admin = Admin(app, name='Gradebook') 
+admin.add_view(UserView(User, db.session)) 
+
+db.create_all()
 # Add administrative views here
 app.run() 
