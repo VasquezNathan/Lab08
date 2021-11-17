df2340b1e43677f78160e13f5545959301885411
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2021-11-16 19:12:55 -0800

    what the fuck am i doing

--- api.py
+++ api.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, request, Response, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite" 
 db = SQLAlchemy(app) 
 
+
+# create the user table with schema user(id, username, password)
 class User(db.Model): 
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, auto_increment = True) 
     username = db.Column(db.String, unique=True, nullable=False) 
-    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, unique=True, nullable=False)
+
+
+    # flask login integration
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # something to do with admin interface
+    def check_password(self, password): 
+        return self.password == password
--- app.py
+++ app.py
@@ -1,14 +1,55 @@
-from flask_admin import Admin 
+from os import abort
+from flask.helpers import url_for
+from flask_admin import Admin
 from flask_admin.contrib import sqla
+from flask_login import LoginManager, current_user, login_user, login_required
+from flask_login.utils import login_required
+from werkzeug.utils import redirect
+from werkzeug.wrappers import response
 from api import *
 
-app.secret_key = 'super secret key'
 
 # set optional bootswatch theme
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.sqlite'
-# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
-# admin.add_view(ModelView(User, db.session)) 
+app.secret_key = 'super secret key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader 
+def load_user(user_id): 
+   return User.get_id(user_id)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # print(User.query.filter_by(username=username).first())   /  
+        if User.query.filter_by(username=username).first() is not None and password == User.query.filter_by(username=username).first().password:
+            # User.query.filter_by(username=username).first().id
+            login_user(User.query.filter_by(username=username).first())
+            return redirect('admin')
+        else:
+            return redirect(url_for('login'))
+    else:
+        return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        '''
+
+
+
+@app.route('/admin')
+@login_required
+def hello():
+    return response.Response(status=200)
 
 class UserView(sqla.ModelView): 
     can_delete = False # disable model deletion 
